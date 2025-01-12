40e9f4eff90ae797ee81bee794938f84d64c1e69
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-12 10:04:50 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,20 +1,26 @@
-chrome.action.onClicked.addListener(async () =>
-  chrome.action.setIcon({
-    path: (await chrome.scripting.getRegisteredContentScripts()).length
-      ? (chrome.scripting.unregisterContentScripts(), "off.png")
-      : (chrome.scripting.registerContentScripts([{
-          id: "0",
-          css: ["main.css"],
-          matches: ["<all_urls>"],
-          runAt: "document_start",
-          allFrames: !0
-         }]),
-         "on.png")
-  })
+chrome.action.onClicked.addListener(() =>
+  chrome.scripting.getRegisteredContentScripts(scripts => 
+    chrome.action.setIcon({
+      path: scripts.length
+        ? (
+          chrome.scripting.unregisterContentScripts(),
+          "off.png"
+        )
+        : (
+          chrome.scripting.registerContentScripts([{
+            id: "0",
+            css: ["main.css"],
+            matches: ["<all_urls>"],
+            runAt: "document_start",
+            allFrames: !0
+          }]),
+         "on.png"
+        )
+    })
+  )
 );
-chrome.scripting.getRegisteredContentScripts().then(scripts =>
-  scripts.length ||
-  chrome.scripting.registerContentScripts([{
+chrome.scripting.getRegisteredContentScripts(scripts =>
+  scripts.length || chrome.scripting.registerContentScripts([{
     id: "0",
     css: ["main.css"],
     matches: ["<all_urls>"],
