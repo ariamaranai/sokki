57c15b36beadddff0b2533c57eb588eb1adf3c65
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-01 17:30:57 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,32 +1,36 @@
-chrome.action.onClicked.addListener(async () =>
-  chrome.action.setIcon({
-    path: (await chrome.scripting.getRegisteredContentScripts()).length
-      ? (
-        await chrome.scripting.unregisterContentScripts(),
-        "off.png"
-      )
-      : (
-        await chrome.scripting.registerContentScripts([{
-          id: "0",
-          css: ["main.css"],
-          matches: ["<all_urls>"],
-          runAt: "document_start",
-          allFrames: !0
-        }]),
-        "on.png"
-      )
-  })
-);
-chrome.runtime.onStartup.addListener(async () =>
-  (await chrome.scripting.getRegisteredContentScripts()).length || (
-    await chrome.scripting.registerContentScripts([{
-      id: "0",
-      css: ["main.css"],
-      matches: ["<all_urls>"],
-      runAt: "document_start",
-      allFrames: !0
-    }]),
-    chrome.action.setIcon({ path: "on.png" })
-  )
-);
+chrome.action.onClicked.addListener(async () => {
+  try {
+    chrome.action.setIcon({
+      path: (await chrome.scripting.getRegisteredContentScripts()).length
+        ? (
+          await chrome.scripting.unregisterContentScripts(),
+          "off.png"
+        )
+        : (
+          await chrome.scripting.registerContentScripts([{
+            id: "0",
+            css: ["main.css"],
+            matches: ["<all_urls>"],
+            runAt: "document_start",
+            allFrames: !0
+          }]),
+          "on.png"
+        )
+    })
+  } catch (e) {}
+});
+chrome.runtime.onStartup.addListener(async () => {
+  try {
+    (await chrome.scripting.getRegisteredContentScripts()).length || (
+      await chrome.scripting.registerContentScripts([{
+        id: "0",
+        css: ["main.css"],
+        matches: ["<all_urls>"],
+        runAt: "document_start",
+        allFrames: !0
+      }]),
+      chrome.action.setIcon({ path: "on.png" })
+    )
+  } catch (e) {}
+});
 chrome.runtime.onStartup.dispatch();
