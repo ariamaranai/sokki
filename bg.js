c1e0e245441c69e893a66432b866920f9b9f06de
a <106607480+ariamaranai@users.noreply.github.com>
2025-07-17 00:06:12 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,40 +1,35 @@
 chrome.action.onClicked.addListener(async () => {
   try {
-    chrome.action.setIcon({
-      path: (await chrome.scripting.getRegisteredContentScripts()).length
-        ? (
-          await chrome.scripting.unregisterContentScripts(),
-          "off.png"
-        )
-        : (
-          await chrome.scripting.registerContentScripts([{
-            id: "0",
-            css: ["main.css"],
-            matches: ["<all_urls>"],
-            runAt: "document_start",
-            allFrames: !0
-          }]),
-          "on.png"
-        )
-    })
+    (await chrome.scripting.getRegisteredContentScripts()).length
+      ? (
+        await chrome.scripting.unregisterContentScripts(),
+        chrome.action.setIcon({
+          path: "off.png"
+        })
+      )
+      : chrome.runtime.onInstalled.dispatch();
   } catch {}
 });
 {
   let isCalled;
   chrome.runtime.onStartup.addListener(async () => {
     try {
-      isCalled ??=
-        (await chrome.scripting.getRegisteredContentScripts()).length || (
-          await chrome.scripting.registerContentScripts([{
-            id: "0",
-            css: ["main.css"],
-            matches: ["<all_urls>"],
-            runAt: "document_start",
-            allFrames: !0
-          }]),
-          chrome.action.setIcon({ path: "on.png" })
-        )
+      isCalled ??= chrome.action.setIcon({
+        path: (await chrome.scripting.getRegisteredContentScripts()).length ? "on.png" : "off.png"
+      });
     } catch {}
   });
 }
+chrome.runtime.onInstalled.addListener(() => (
+  chrome.scripting.registerContentScripts([{
+    id: "0",
+    css: ["main.css"],
+    matches: ["<all_urls>"],
+    runAt: "document_start",
+    allFrames: !0
+  }]),
+  chrome.action.setIcon({
+    path: "on.png"
+  })
+));
 chrome.runtime.onStartup.dispatch();
